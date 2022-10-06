140c362f18736a16c61c2d3deeed9f297582ccd1
Vinicios Santos <95624273+vinicios-santos@users.noreply.github.com>
2022-10-06 20:16:47 -0300

    auxilio para desenvolvimento

---
+++ Academia-main/src/main/java/com/estruturadados/academia/utils/Utils.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.estruturadados.academia.utils;
+
+import com.estruturadados.academia.ghrapic.MainView;
+import java.awt.Dimension;
+
+/**
+ *
+ * @author lab201a
+ */
+public class Utils {
+    
+}
